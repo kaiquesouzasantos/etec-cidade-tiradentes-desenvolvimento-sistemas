05520fc9c32b7646bccda0af5f781237f5018c21
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-02-10 22:21:44 -0300

    Add files via upload

---
+++ Revisao/src/Revisao/MatrizRevisao.java
@@ -0,0 +1,25 @@
+package Revisao;
+import java.util.*;
+
+public class MatrizRevisao {
+    public Scanner in = new Scanner(System.in);
+    public double matriz[][] = new double[3][4];
+    public String aluno[] = {"A","B","C"};
+
+    public void matriz(){
+        for(int i = 0; i < 3; i++){
+            for(int k = 0; k < 4; k++){
+                System.out.print("\u001B[31mDigite uma nota: ");
+                matriz[i][k] = in.nextDouble();
+            }
+        }
+        System.out.println("NOTAS");
+        for(int i = 0; i < 3; i++){
+            System.out.print("\u001B[33mALUNO: \u001B[32m"+aluno[i]+"\u001B[33m | NOTAS: ");
+            for(int k = 0; k < 4; k++){
+                System.out.print("\u001B[36m"+matriz[i][k]+"  ");
+            }
+            System.out.println("\033[0m");
+        }
+    }
+}
---
+++ Revisao/src/Revisao/VetorRevisao.java
@@ -0,0 +1,12 @@
+package Revisao;
+
+public class VetorRevisao {
+    public int vetor[] = {3,2,8,7,5,4};
+
+    public void outVetor(){
+        for(int i = 0; i < vetor.length; i++){
+            System.out.println("\u001B[34mNa posicao \u001B[32m"+i+"\u001B[34m teremos o valor \u001B[36m"+vetor[i]);
+        }
+    }
+
+}
