7a27300d281ea94cb176cd9eb9e118b323d943f0
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-02-13 17:16:07 -0300

    Add files via upload

--- Revisao/src/Revisao/Revisao.java
+++ Revisao/src/Revisao/Revisao.java
@@ -4,6 +4,18 @@ import java.util.*;
 public class Revisao {
     public static final Scanner in = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        System.out.print("\u001B[36m=============== PROCEDIMENTOS POSSIVEIS ===============" +
+                "\n\u001B[33m[1] - Exercicio 1 e 2 com Estrutura de Decisao" +
+                "\n[2] - Exercicio 3 com Estrutura de Repeticao" +
+                "\n[3] - Desafio" +
+                "\n[4] - Exercicio com Matriz" +
+                "\n[5] - Exercicio com Vetor" +
+                "\n\n\u001B[31mQual procedimento deseja realizar: ");
+        int opcaoProcedimento = in.nextInt();
+        procedimentosRevisao(opcaoProcedimento);
+    }
+
     public static void procedimentosRevisao(int opcaoProcedimento){
         switch(opcaoProcedimento){
             case 1:
@@ -37,17 +49,5 @@ public class Revisao {
             default:
                 System.out.println("\033[0m\nOPCAO INVALIDA, PROGRAMA ENCERRADO!");
         }
-    }
-
-    public static void main(String[] args) {
-        System.out.print("\u001B[36m=============== PROCEDIMENTOS POSSIVEIS ===============" +
-                "\n\u001B[33m[1] - Exercicio 1 e 2 com Estrutura de Decisao" +
-                "\n[2] - Exercicio 3 com Estrutura de Repeticao" +
-                "\n[3] - Desafio" +
-                "\n[4] - Exercicio com Matriz" +
-                "\n[5] - Exercicio com Vetor" +
-                "\n\n\u001B[31mQual procedimento deseja realizar: ");
-        int opcaoProcedimento = in.nextInt();
-        procedimentosRevisao(opcaoProcedimento);
     }
 }
