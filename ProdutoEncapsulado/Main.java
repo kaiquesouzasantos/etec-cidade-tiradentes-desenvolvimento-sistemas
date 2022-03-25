bc80d4444c18c0c358c996e55f45e523ca4306e5
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-03-25 20:05:12 -0300

    Update Main.java

--- ProdutoEncapsulado/Main.java
+++ ProdutoEncapsulado/Main.java
@@ -16,10 +16,6 @@ public class Main {
 		}while(!produto.precoVendaValidacao);
 
 		produto.calcularMargemLucro();
-		System.out.print("\nNOME DO PRODUTO: "+produto.getNomeProduto());
-		System.out.print("\nVALOR DE CUSTO: R$"+produto.getPrecoCusto());
-		System.out.print("\nVALOR DE VENDA: R$"+produto.getPrecoVenda());
-		System.out.print("\nMARGEM DE LUCRO: R$"+produto.getMargemLucro());
-		System.out.print("\nPERCENTUAL DE MARGEM DE LUCRO: "+produto.getMargemLucroPorcentagem()+"%");
+		System.out.print("\nNOME DO PRODUTO: "+produto.getNomeProduto()+"\nVALOR DE CUSTO: R$"+produto.getPrecoCusto()+"\nVALOR DE VENDA: R$"+produto.getPrecoVenda()+"\nMARGEM DE LUCRO: R$"+produto.getMargemLucro()+"\nPERCENTUAL DE MARGEM DE LUCRO: "+produto.getMargemLucroPorcentagem()+"%");
 	}
 }
