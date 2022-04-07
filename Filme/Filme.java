daf9f84062cea97c66aa56d8039a8c5e31e35a02
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-04-07 08:40:49 -0300

    Update Filme.java

--- Filme/Filme.java
+++ Filme/Filme.java
@@ -5,10 +5,12 @@ public class Filme {
     private int duracaoEmMinutos;
 
     public Filme(){}
-
+    
+    // SET's
     public void setTituloFilme(String tituloFilme){this.tituloFilme = tituloFilme;}
     public void setDuracaoEmMinutos(int duracaoEmMinutos){this.duracaoEmMinutos = duracaoEmMinutos;}
 
+    // GET's
     public String getTituloFilme(){return this.tituloFilme;}
     public String exibirDuracaoEmHoras(){return (this.duracaoEmMinutos/60)+"h "+(this.duracaoEmMinutos%60)+"min";}
 }
