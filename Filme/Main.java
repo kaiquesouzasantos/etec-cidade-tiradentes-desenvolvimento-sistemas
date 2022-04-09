b2a84942f48673fe7bf4bed0a967c16ed8264f56
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-04-09 07:06:48 -0300

    Update Main.java

--- Filme/Main.java
+++ Filme/Main.java
@@ -2,6 +2,11 @@ package Filme;
 import javax.swing.*;
 
 public class Main {
+    public static void recebeFilmes(Filme filme){
+        filme.setTituloFilme(JOptionPane.showInputDialog(null, "Digite o nome do filme: "));
+        filme.setDuracaoEmMinutos(Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a duracao do filme: ")));
+    }
+
     public static void main(String[] args) {
         // 01 - FILME
         Filme filme1 = new Filme();
@@ -13,34 +18,22 @@ public class Main {
         filme2.setTituloFilme("Hotel Transilvania");
         filme2.setDuracaoEmMinutos(93);
 
-        System.out.println("Os filme em cartazs são "+filme1.getTituloFilme()+" e "+filme2.getTituloFilme());
+        System.out.println("Os filme em cartazs sao "+filme1.getTituloFilme()+" e "+filme2.getTituloFilme());
 
-        // 03
+        // 03 | 08 - FILMES
         Filme filme3 = new Filme();
-        filme3.setTituloFilme(JOptionPane.showInputDialog(null, "Digite o nome do filme: "));
-        filme3.setDuracaoEmMinutos(Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a duracao do filme: ")));
-
-        // 04
         Filme filme4 = new Filme();
-        filme4.setTituloFilme(JOptionPane.showInputDialog(null, "Digite o nome do filme: "));
-        filme4.setDuracaoEmMinutos(Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a duracao do filme: ")));
-
-        // 05
         Filme filme5 = new Filme();
-        filme5.setTituloFilme(JOptionPane.showInputDialog(null, "Digite o nome do filme: "));
-        filme5.setDuracaoEmMinutos(Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a duracao do filme: ")));
-
-        // 06
         Filme filme6 = new Filme();
-        filme6.setTituloFilme(JOptionPane.showInputDialog(null, "Digite o nome do filme: "));
-        filme6.setDuracaoEmMinutos(Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a duracao do filme: ")));
-
-        // 07
         Filme filme7 = new Filme();
-        filme7.setTituloFilme(JOptionPane.showInputDialog(null, "Digite o nome do filme: "));
-        filme7.setDuracaoEmMinutos(Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a duracao do filme: ")));
 
-        JOptionPane.showMessageDialog(null, "Os filme em cartazs são: \n\nDuracao | Nome\n\n"+
+        recebeFilmes(filme3);
+        recebeFilmes(filme4);
+        recebeFilmes(filme5);
+        recebeFilmes(filme6);
+        recebeFilmes(filme7);
+
+        JOptionPane.showMessageDialog(null, "Os filme em cartazs sao: \n\nDURACAO | NOME\n\n"+
                 filme1.exibirDuracaoEmHoras()+"  |  "+filme1.getTituloFilme()+"\n"+
                 filme2.exibirDuracaoEmHoras()+"  |  "+filme2.getTituloFilme()+"\n"+
                 filme3.exibirDuracaoEmHoras()+"  |  "+filme3.getTituloFilme()+"\n"+
