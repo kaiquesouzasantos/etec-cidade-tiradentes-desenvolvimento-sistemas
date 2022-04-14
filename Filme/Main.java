8b2967d6fc3c0a2c9d28b4a6074b987c8f37f457
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-04-14 18:16:56 -0300

    Update Main.java

--- Filme/Main.java
+++ Filme/Main.java
@@ -2,6 +2,8 @@ package Filme;
 import javax.swing.*;
 
 public class Main {
+    public static Filme filme3, filme4, filme5, filme6, filme7;
+    
     public static void recebeFilmes(Filme filme){
         filme.setTituloFilme(JOptionPane.showInputDialog(null, "Digite o nome do filme: "));
         filme.setDuracaoEmMinutos(Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a duracao do filme: ")));
@@ -21,18 +23,12 @@ public class Main {
         System.out.println("Os filme em cartazs sao "+filme1.getTituloFilme()+" e "+filme2.getTituloFilme());
 
         // 03 | 08 - FILMES
-        Filme filme3 = new Filme();
-        Filme filme4 = new Filme();
-        Filme filme5 = new Filme();
-        Filme filme6 = new Filme();
-        Filme filme7 = new Filme();
-
         recebeFilmes(filme3);
         recebeFilmes(filme4);
         recebeFilmes(filme5);
         recebeFilmes(filme6);
         recebeFilmes(filme7);
-
+        
         JOptionPane.showMessageDialog(null, "Os filme em cartazs sao: \n\nDURACAO | NOME\n\n"+
                 filme1.exibirDuracaoEmHoras()+"  |  "+filme1.getTituloFilme()+"\n"+
                 filme2.exibirDuracaoEmHoras()+"  |  "+filme2.getTituloFilme()+"\n"+
