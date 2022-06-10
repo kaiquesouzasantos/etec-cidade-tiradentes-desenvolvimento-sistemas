32476085b4e398a893bf8f3488c9b5fdcd77586d
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-06-10 04:11:53 -0300

    Add files via upload

---
+++ FuncionarioHeranca/Funcionario.java
@@ -0,0 +1,21 @@
+package FuncionarioHeranca;
+
+public class Funcionario {
+    private String nome, cpf;
+    private double salario;
+
+    // SET's
+    public void setNome(String nome) {this.nome = nome;}
+    public void setCpf(String cpf) {this.cpf = cpf;}
+    public void setSalario(double salario) {this.salario = salario;}
+
+    // GET's
+    public String getNome() {return nome;}
+    public String getCpf() {return cpf;}
+    public double getSalario() {return salario;}
+
+    // VALOR COMISSAO
+    public double valorComissao(){
+        return this.salario * 0.1;
+    }
+}
