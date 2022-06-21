b375c047a1ebd924fac0700db462fa60478c92dc
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-06-21 14:54:10 -0300

    Add files via upload

---
+++ Animal/Entidade/Animal.java
@@ -0,0 +1,11 @@
+package Animal.Entidade;
+
+public class Animal {
+	public String especie, nome;
+	public int quantPatas;
+	public double peso;
+	
+	public void imprimirValoresAnimal() {
+		System.out.println("\nNOME: "+this.nome+"\nESPECIE: "+this.especie+"\nQUANTIDADE DE PATAS: "+this.quantPatas+"\nPESO: "+this.peso+"KG");
+	}
+}
--- Animal/Main.java
+++ Animal/Main.java
@@ -1,45 +1,16 @@
 package Animal;
-import java.util.Scanner;
+import Animal.Entidade.Animal;
+import javax.swing.*;
 
 public class Main {
-	public static final Scanner in = new Scanner(System.in);
-	public static Animal primeiroAnimal = new Animal();
-	public static Animal segundoAnimal = new Animal();
-	public static Animal terceiroAnimal = new Animal();
-	
 	public static void main(String[] args) {
-		System.out.print("======== ANIMAL ========");
-		int cont = 0;
-		while(cont < 3) {
-			System.out.print("\nDIGITE O NOME DO ANIMAL: ");
-			String nome = in.next();
-			System.out.print("DIGITE A ESPÉCIE: ");
-			String especie = in.next();
-			System.out.print("DIGITE A QUANTIDADE DE PATAS: ");
-			int quantPatas = in.nextInt();
-			System.out.print("DIGITE O PESO EM KG(QUILOGRAMA): ");
-			double peso = in.nextDouble();
-			
-			if(cont == 0) {
-				primeiroAnimal.nome = nome;
-				primeiroAnimal.especie = especie;
-				primeiroAnimal.quantPatas = quantPatas;
-				primeiroAnimal.peso = peso;
-			}else if(cont == 1) {
-				segundoAnimal.nome = nome;
-				segundoAnimal.especie = especie;
-				segundoAnimal.quantPatas = quantPatas;
-				segundoAnimal.peso = peso;
-			}else if(cont == 2){
-				terceiroAnimal.nome = nome;
-				terceiroAnimal.especie = especie;
-				terceiroAnimal.quantPatas = quantPatas;
-				terceiroAnimal.peso = peso;
-			}
-			cont++;
-		}
-		primeiroAnimal.imprimirValoresAnimal();
-		segundoAnimal.imprimirValoresAnimal();
-		terceiroAnimal.imprimirValoresAnimal();
+		Animal animal = new Animal();
+
+		animal.nome = JOptionPane.showInputDialog("DIGITE O NOME DO ANIMAL: ");
+		animal.especie = JOptionPane.showInputDialog("DIGITE A ESPÉCIE: ");
+		animal.quantPatas = Integer.parseInt(JOptionPane.showInputDialog("DIGITE A QUANTIDADE DE PATAS: "));
+		animal.peso = Double.parseDouble(JOptionPane.showInputDialog("DIGITE O PESO EM KG(QUILOGRAMA): "));
+
+		animal.imprimirValoresAnimal();
 	}
 }
