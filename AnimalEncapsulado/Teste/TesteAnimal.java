5407166908fdef04b0785768ad8d20e4ee460bdc
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-06-21 15:25:01 -0300

    Add files via upload

--- AnimalEncapsulado/Teste/TesteAnimal.java
+++ AnimalEncapsulado/Teste/TesteAnimal.java
@@ -1,27 +1,28 @@
 package AnimalEncapsulado.Teste;
+
 import AnimalEncapsulado.Entidade.Animal;
 
 public class TesteAnimal {
 	public static void main(String[] args) {
 		// TESTE-01
 		Animal primeiroAnimal = new Animal();
-		primeiroAnimal.setEspecie("Mamifero");
-		primeiroAnimal.setNome("Tamandua");
+		primeiroAnimal.setEspecie("animal_01");
+		primeiroAnimal.setNome("animal_01");
 		primeiroAnimal.setQuantPatas(4);
 		primeiroAnimal.setPeso(30);
 
 		// TESTE-02
 		Animal segundoAnimal = new Animal();
-		segundoAnimal.setEspecie("Mamifero");
-		segundoAnimal.setNome("Girafa");
+		segundoAnimal.setEspecie("animal_02");
+		segundoAnimal.setNome("animal_02");
 		segundoAnimal.setQuantPatas(4);
 		segundoAnimal.setPeso(1200);
 
 		// TESTE-03
-		Animal terceiroAnimal = new Animal("Reptil", "Jacare", 4, 300);
+		Animal terceiroAnimal = new Animal("animal_03", "animal_03", 4, 300);
 		
 		// TESTE-04
-		Animal quartoAnimal = new Animal("Mamifero", "Capivara ", 4, 50);
+		Animal quartoAnimal = new Animal("animal_04", "animal_04 ", 4, 50);
 
 		// SAIDA
 		System.out.println("----------------------------------------------------------------------------------");
