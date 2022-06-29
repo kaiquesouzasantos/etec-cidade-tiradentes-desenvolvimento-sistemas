a5713a772ff18e116d2dc51f64a18c5020b86118
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-06-29 17:49:37 -0300

    Add files via upload

--- Retangulo/Main.java
+++ Retangulo/Main.java
@@ -1,31 +1,14 @@
 package Retangulo;
-import java.util.Scanner;
+import javax.swing.*;
 
 public class Main {
-    public static final Scanner sc = new Scanner(System.in);
-    public static Retangulo retangulo1 = new Retangulo();
-    public static Retangulo retangulo2 = new Retangulo();
     public static void main(String[] args) {
-        double base, altura;
-        int i = 0;
-        System.out.println("CALCULO DA AREA DO RETANGULO\n");
+        Retangulo retangulo = new Retangulo();
+        for(int i = 0; i < 3; i++){
+            retangulo.baseRetangulo = Double.parseDouble(JOptionPane.showInputDialog("DIGITE O VALOR DA BASE: "));
+            retangulo.alturaRetangulo = Double.parseDouble(JOptionPane.showInputDialog("DIGITE O VALOR DA ALTURA: "));
 
-        while(i < 2){
-            System.out.print("DIGITE O VALOR DA BASE: ");
-            base = sc.nextDouble();
-            System.out.print("DIGITE O VALOR DA ALTURA: ");
-            altura = sc.nextDouble();
-
-            if(i == 0){
-                retangulo1.baseRetangulo = base;
-                retangulo1.alturaRetangulo = altura;
-                System.out.println("A AREA DO RETANGULO1 É IGUAL Á "+retangulo1.calcularArea()+"\n");
-            }else{
-                retangulo2.baseRetangulo = base;
-                retangulo2.alturaRetangulo = altura;
-                System.out.println("A AREA DO RETANGULO2 É IGUAL Á "+retangulo2.calcularArea());
-            }
-            i++;
+            JOptionPane.showMessageDialog(null, "A AREA DO RETANGULO É IGUAL Á "+retangulo.calcularArea());
         }
     }
 }
