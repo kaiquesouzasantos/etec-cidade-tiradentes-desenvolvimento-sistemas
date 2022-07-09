cf9357e0122938312af3b9b4ac2e4260b7ee87ed
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-07-09 06:38:00 -0300

    Add files via upload

--- VeiculoPolimorfismo/Main.java
+++ VeiculoPolimorfismo/Main.java
@@ -1,13 +1,34 @@
 package VeiculoPolimorfismo;
 import VeiculoPolimorfismo.Entidades.*;
+
+import javax.swing.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        executaParticularidades(List.of(
-                new Carro("Triton L200", "Black Piano", "Gasolina", 200000),
-                new Motocicleta("R1200", "Preto", "Gasolina", 75000)
-        ));
+        List<Veiculo> veiculos = new ArrayList<>();
+        int continua;
+
+        do{
+            int opcao = Integer.parseInt(JOptionPane.showInputDialog("[1] - CARRO\n[2] - MOTOCICLETA\n\nDIGITE O TIPO DE VEICULO: "));
+            if(opcao != 1 && opcao != 2){System.exit(0);}
+
+            String modelo = JOptionPane.showInputDialog("DIGITE O MODELO: ");
+            String cor = JOptionPane.showInputDialog("DIGITE A COR: ");
+            String combustivel = JOptionPane.showInputDialog("DIGITE O TIPO DE COMBUSTIVEL: ");
+            double valor = Double.parseDouble(JOptionPane.showInputDialog("DIGITE O VALOR: R$"));
+
+            if(opcao == 1){
+                veiculos.add(new Carro(modelo,cor,combustivel,valor));
+            }else {
+                veiculos.add(new Motocicleta(modelo,cor,combustivel,valor));
+            }
+
+            continua = JOptionPane.showConfirmDialog(null, "DESEJA CONTINUAR CADASTRANDO VEICULOS?");
+        }while (continua == JOptionPane.YES_NO_OPTION);
+
+        executaParticularidades(veiculos);
     }
 
     public static void executaParticularidades(List<? extends Veiculo> veiculos){
