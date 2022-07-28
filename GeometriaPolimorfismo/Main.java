2da69acec3210e439c93aed8b9711d93ccb92e40
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-07-28 19:42:29 -0300

    Update Main.java

--- GeometriaPolimorfismo/Main.java
+++ GeometriaPolimorfismo/Main.java
@@ -1,30 +1,41 @@
 package GeometriaPolimorfismo;
 import GeometriaPolimorfismo.Entidades.*;
 import javax.swing.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        int opcao = Integer.parseInt(JOptionPane.showInputDialog(
-                "[1] - Ciruculo\n[2] - Retangulo\n[3] - Losangulo\n[4] - Triangulo\n[5] - Quadrado\n\nDIGITE O NUMERO DA FIGURA GEOMETRICA:"));
-        if (opcao != 1 && opcao != 2 && opcao != 3 && opcao != 4 && opcao != 5) System.exit(0);
+        String continua, saida = "";
+        List<FiguraGeometrica> figuras = new ArrayList<>();
 
-        if(opcao == 1){
-            aprensentaFiguraGeometrica(new Circulo(
-                    Double.parseDouble(JOptionPane.showInputDialog("DIGITE O RAIO: "))));
-        } else if(opcao == 5) {
-            aprensentaFiguraGeometrica(new Quadrado(
-                    Double.parseDouble(JOptionPane.showInputDialog("DIGITE O TAMANHO DO LADO: "))));
-        } else{
-            double largura = Double.parseDouble(JOptionPane.showInputDialog("DIGITE A LARGURA: "));
-            double altura = Double.parseDouble(JOptionPane.showInputDialog("DIGITE A ALTURA: "));
-            
-            if(opcao == 2) aprensentaFiguraGeometrica(new Retangulo(altura, largura));
-            else if(opcao == 3) aprensentaFiguraGeometrica(new Losangulo(altura, largura));
-            else aprensentaFiguraGeometrica(new Triangulo(altura, largura));
+        do{
+            int opcao = Integer.parseInt(JOptionPane.showInputDialog(
+                    "[1] - Ciruculo\n[2] - Retangulo\n[3] - Losangulo\n[4] - Triangulo\n[5] - Quadrado\n\nDIGITE A FIGURA QUE DESEJA CALCULAR:"));
+            if (opcao != 1 && opcao != 2 && opcao != 3 && opcao != 4 && opcao != 5) System.exit(0);
+
+            if(opcao == 1){
+                figuras.add(new Circulo(Double.parseDouble(JOptionPane.showInputDialog("DIGITE O RAIO: "))));
+            }else if(opcao == 5) {
+                figuras.add(new Quadrado(Double.parseDouble(JOptionPane.showInputDialog("DIGITE O TAMANHO DO LADO: "))));
+            }else{
+                double largura = Double.parseDouble(JOptionPane.showInputDialog("DIGITE A LARGURA: "));
+                double altura = Double.parseDouble(JOptionPane.showInputDialog("DIGITE A ALTURA: "));
+
+                if(opcao == 2)
+                    figuras.add(new Retangulo(altura, largura));
+                else if(opcao == 3)
+                    figuras.add(new Losangulo(altura, largura));
+                else
+                    figuras.add(new Triangulo(altura, largura));
+            }
+
+            continua = JOptionPane.showInputDialog("DESEJA CONTINUAR? [S/N]");
+        }while(continua.equalsIgnoreCase("S"));
+
+        for (FiguraGeometrica n : figuras) {
+            saida += n + "\n";
         }
-    }
-
-    public static void aprensentaFiguraGeometrica(FiguraGeometrica figura){
-        JOptionPane.showMessageDialog(null, figura);
+        JOptionPane.showMessageDialog(null, saida);
     }
 }
