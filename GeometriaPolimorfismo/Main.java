8a2609cff46e674af74f57050ff4e2daa9b78d9f
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-07-28 07:51:43 -0300

    Update Main.java

--- GeometriaPolimorfismo/Main.java
+++ GeometriaPolimorfismo/Main.java
@@ -1,11 +1,9 @@
 package GeometriaPolimorfismo;
 import GeometriaPolimorfismo.Entidades.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        /*
         List<FiguraGeometrica> figuraGeometricas = List.of(
                 new Circulo(10),
                 new Quadrado(15),
@@ -13,15 +11,6 @@ public class Main {
                 new Triangulo(5, 5),
                 new Losangulo(8, 10)
         );
-        */
-
-        List<FiguraGeometrica> figuraGeometricas = new ArrayList<>();
-
-        figuraGeometricas.add(new Circulo(10));
-        figuraGeometricas.add(new Quadrado(15));
-        figuraGeometricas.add(new Retangulo(12, 20));
-        figuraGeometricas.add(new Triangulo(5, 5));
-        figuraGeometricas.add( new Losangulo(8, 10));
 
         figuraGeometricas.forEach(System.out::println);
     }
