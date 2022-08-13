a29e8ff1f54b7bcd453ce7bdddf7fa7ba598c242
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-08-13 11:35:16 -0300

    Add files via upload

--- Conta/Main.java
+++ Conta/Main.java
@@ -4,32 +4,58 @@ import javax.swing.*;
 
 public class Main {
 	public static void main(String[] args) {
-		int opcao = Integer.parseInt(JOptionPane.showInputDialog("[1] - CONTA COMUM  |  \n[2] - CONTA CORRENTE\n\nDIGITE O TIPO DA CONTA: "));
-		if(opcao != 1 && opcao != 2) System.exit(0);
+		int opcao = Integer.parseInt(JOptionPane.showInputDialog(
+				null, "[1] - CONTA COMUM\n[2] - CONTA CORRENTE\n\nDIGITE O TIPO DA CONTA: ",
+				"MENU", JOptionPane.INFORMATION_MESSAGE));
 
-		String titular = JOptionPane.showInputDialog("DIGITE O TITULAR DA CONTA: ");
-		int numConta = Integer.parseInt(JOptionPane.showInputDialog("DIGITE O NUMERO DA CONTA: "));
-		int agencia = Integer.parseInt(JOptionPane.showInputDialog("DIGITE O NUMERO DA AGENCIA: "));
+		if(opcao != 1 && opcao != 2) {
+			JOptionPane.showMessageDialog(
+					null, "OPCAO INVALIDA!", "MENU", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
 
-		String operacao = JOptionPane.showInputDialog("[S] - SACAR  |  [D] - DEPOSITAR\nDIGITE A OPERACAO DESEJADA: ");
-		double valorTransacao = Double.parseDouble(JOptionPane.showInputDialog("DIGITE O VALOR DA TRANSACAO: R$"));
+		String titular = JOptionPane.showInputDialog(
+				null, "DIGITE O TITULAR DA CONTA: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE);
+
+		String operacao = JOptionPane.showInputDialog(
+				null, "[S] - SACAR  |  [D] - DEPOSITAR\nDIGITE A OPERACAO DESEJADA: ",
+				"OPERACAO", JOptionPane.QUESTION_MESSAGE);
+		double valorTransacao = Double.parseDouble(JOptionPane.showInputDialog(
+				null, "DIGITE O VALOR DA TRANSACAO: R$", "OPERACAO", JOptionPane.QUESTION_MESSAGE));
 
 		if(opcao == 1){
-			Conta contaComum = new Conta(1000, agencia, numConta, titular);
+			Conta contaComum = new Conta(1000, geraNumAgenciaConta(100000), geraNumAgenciaConta(1000000000), titular);
 
-			if(operacao.equalsIgnoreCase("S")) JOptionPane.showMessageDialog(null, contaComum.sacar(valorTransacao));
-			else if(operacao.equalsIgnoreCase("D")) contaComum.depositar(valorTransacao);
+			if(operacao.equalsIgnoreCase("S")) {
+				JOptionPane.showMessageDialog(
+						null, contaComum.sacar(valorTransacao), "OPERACAO", JOptionPane.INFORMATION_MESSAGE);
+			}else if(operacao.equalsIgnoreCase("D")) {
+				contaComum.depositar(valorTransacao);
+				JOptionPane.showMessageDialog(
+						null, "DEPOSITO REALIZADO COM SUCESSO", "OPERACAO", JOptionPane.INFORMATION_MESSAGE);
+			}
 
-			JOptionPane.showMessageDialog(null, contaComum);
+			JOptionPane.showMessageDialog(
+					null, contaComum, "CONTA", JOptionPane.INFORMATION_MESSAGE);
 
 		}else{
-			ContaCorrente contaCorrente = new ContaCorrente(1000, 2000, agencia, numConta, titular);
+			ContaCorrente contaCorrente = new ContaCorrente(1000, 2000,  geraNumAgenciaConta(100000), geraNumAgenciaConta(1000000000), titular);
 
-			if(operacao.equalsIgnoreCase("S")) JOptionPane.showMessageDialog(null, contaCorrente.sacar(valorTransacao));
-			else if(operacao.equalsIgnoreCase("D")) contaCorrente.depositar(valorTransacao);
+			if(operacao.equalsIgnoreCase("S")) {
+				JOptionPane.showMessageDialog(
+						null, contaCorrente.sacar(valorTransacao), "OPERACAO", JOptionPane.INFORMATION_MESSAGE);
+			}else if(operacao.equalsIgnoreCase("D")) {
+				contaCorrente.depositar(valorTransacao);
+				JOptionPane.showMessageDialog(
+						null, "DEPOSITO REALIZADO COM SUCESSO", "OPERACAO", JOptionPane.INFORMATION_MESSAGE);
+			}
 			contaCorrente.taxarConta();
 
-			JOptionPane.showMessageDialog(null, contaCorrente);
+			JOptionPane.showMessageDialog(null, contaCorrente, "CONTA", JOptionPane.INFORMATION_MESSAGE);
 		}
+	}
+
+	private static int geraNumAgenciaConta(int digitos){
+		return (int) (Math.random() * digitos);
 	}
 }
