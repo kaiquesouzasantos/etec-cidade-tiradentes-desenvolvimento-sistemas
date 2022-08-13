7c3ae0ce11187aab7fd079cacd61519ec7377713
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-08-13 11:22:15 -0300

    Add files via upload

--- Funcionario/Main.java
+++ Funcionario/Main.java
@@ -6,10 +6,12 @@ public class Main {
         Funcionario[] listaFuncionaios = new Funcionario[]{new Funcionario("Pedro"), new Funcionario("Vagner")};
 
         for(Funcionario funcionario:listaFuncionaios){
-            funcionario.salarioBruto = Double.parseDouble(JOptionPane.showInputDialog("DIGITE SEU SALARIO BRUTO: R$"));
-            funcionario.imposto = Double.parseDouble(JOptionPane.showInputDialog("DIGITE A PORCENTAGEM DO IMPOSTO APLICADO: "));
+            funcionario.salarioBruto = Double.parseDouble(JOptionPane.showInputDialog(
+                    null, "DIGITE SEU SALARIO BRUTO: R$", "INFORMACAO", JOptionPane.QUESTION_MESSAGE));
+            funcionario.imposto = Double.parseDouble(JOptionPane.showInputDialog(
+                    null, "DIGITE A PORCENTAGEM DO IMPOSTO APLICADO: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE));
 
-            JOptionPane.showMessageDialog(null, funcionario);
+            JOptionPane.showMessageDialog(null, funcionario, "FUNCIONARIO", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 }
