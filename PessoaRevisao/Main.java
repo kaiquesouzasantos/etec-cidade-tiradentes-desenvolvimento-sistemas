d11e325eb7b0f9e8d0edd70bd5e1c4044cfc7eb5
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-08-13 17:46:03 -0300

    Add files via upload

--- PessoaRevisao/Main.java
+++ PessoaRevisao/Main.java
@@ -1,18 +1,19 @@
 package PessoaRevisao;
 import javax.swing.*;
 
-public class TestePessoa {
+public class Main {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
 
-        pessoa.setNome(JOptionPane.showInputDialog("DIGITE SEU NOME: "));
-        pessoa.setAltura(Double.parseDouble(JOptionPane.showInputDialog("DIGITE SUA ALTURA EM METROS: ")));
-        String frase = JOptionPane.showInputDialog("DIGITE UMA FRASE: ");
+        pessoa.setNome(JOptionPane.showInputDialog(
+                null, "DIGITE SEU NOME: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE));
+        pessoa.setAltura(Double.parseDouble(JOptionPane.showInputDialog(
+                null, "DIGITE SUA ALTURA EM METROS: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE)));
+        String frase = JOptionPane.showInputDialog(
+                null, "DIGITE UMA FRASE: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE);
         pessoa.setFrase(frase);
 
-        JOptionPane.showMessageDialog(null, pessoa.retornaAtributos());
-
-        if(pessoa.getFrase().equalsIgnoreCase("")){JOptionPane.showMessageDialog(null, pessoa.retornaFrase("FRASE DEFUALT"));
-        }else{JOptionPane.showMessageDialog(null, pessoa.retornaFrase());}
+        JOptionPane.showMessageDialog(
+                null, pessoa.retornaAtributos(), "PESSOA", JOptionPane.INFORMATION_MESSAGE);
     }
 }
