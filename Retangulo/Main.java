5a288a3e0bf7a34119fdeb12489e1865d9f34d0d
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-08-13 17:41:36 -0300

    Add files via upload

--- Retangulo/Main.java
+++ Retangulo/Main.java
@@ -5,10 +5,13 @@ public class Main {
     public static void main(String[] args) {
         Retangulo retangulo = new Retangulo();
         for(int i = 0; i < 3; i++){
-            retangulo.baseRetangulo = Double.parseDouble(JOptionPane.showInputDialog("DIGITE O VALOR DA BASE: "));
-            retangulo.alturaRetangulo = Double.parseDouble(JOptionPane.showInputDialog("DIGITE O VALOR DA ALTURA: "));
+            retangulo.baseRetangulo = Double.parseDouble(JOptionPane.showInputDialog(
+                    null, "DIGITE O VALOR DA BASE: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE));
+            retangulo.alturaRetangulo = Double.parseDouble(JOptionPane.showInputDialog(
+                    null, "DIGITE O VALOR DA ALTURA: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE));
 
-            JOptionPane.showMessageDialog(null, "A AREA DO RETANGULO É IGUAL Á "+retangulo.calcularArea());
+            JOptionPane.showMessageDialog(
+                    null, "A AREA DO RETANGULO É IGUAL Á "+retangulo.calcularArea(), "RETANGULO", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 }
