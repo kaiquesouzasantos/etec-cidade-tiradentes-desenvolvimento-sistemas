97e4275e6a446aec0bb308eaba3d87e4a2a76561
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-08-13 11:19:12 -0300

    Add files via upload

--- VeiculoPolimorfismo/Main.java
+++ VeiculoPolimorfismo/Main.java
@@ -10,20 +10,34 @@ public class Main {
         int continua;
 
         do{
-            int opcao = Integer.parseInt(JOptionPane.showInputDialog("[1] - CARRO\n[2] - MOTOCICLETA\n\nDIGITE O TIPO DE VEICULO: "));
-            if(opcao != 1 && opcao != 2){System.exit(0);}
+            int opcao = Integer.parseInt(JOptionPane.showInputDialog(
+                    null, "[1] - CARRO\n[2] - MOTOCICLETA\n\nDIGITE O TIPO DE VEICULO: ",
+                    "MENU", JOptionPane.QUESTION_MESSAGE
+            ));
 
-            String modelo = JOptionPane.showInputDialog("DIGITE O MODELO: ");
-            String cor = JOptionPane.showInputDialog("DIGITE A COR: ");
-            String combustivel = JOptionPane.showInputDialog("DIGITE O TIPO DE COMBUSTIVEL: ");
-            double valor = Double.parseDouble(JOptionPane.showInputDialog("DIGITE O VALOR: R$"));
+            if(opcao != 1 && opcao != 2) {
+                JOptionPane.showMessageDialog(
+                        null, "OPERACAO INVALIDA, CADASTRO DESCONTINUADO!", "MENU", JOptionPane.ERROR_MESSAGE);
+            }else{
+                String modelo = JOptionPane.showInputDialog(
+                        null, "DIGITE O MODELO: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE);
+                String cor = JOptionPane.showInputDialog(
+                        null, "DIGITE A COR: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE);
+                String combustivel = JOptionPane.showInputDialog(
+                        null, "DIGITE O TIPO DE COMBUSTIVEL: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE);
+                double valor = Double.parseDouble(JOptionPane.showInputDialog(
+                        null,"DIGITE O VALOR: R$", "INFORMACAO", JOptionPane.QUESTION_MESSAGE));
 
-            if(opcao == 1) veiculos.add(new Carro(modelo,cor,combustivel,valor));
-            else veiculos.add(new Motocicleta(modelo,cor,combustivel,valor));
+                if(opcao == 1){
+                    veiculos.add(new Carro(modelo,cor,combustivel,valor));
+                }else {
+                    veiculos.add(new Motocicleta(modelo,cor,combustivel,valor));
+                }
+            }
 
             continua = JOptionPane.showConfirmDialog(null, "DESEJA CONTINUAR CADASTRANDO VEICULOS?");
         }while (continua == JOptionPane.YES_NO_OPTION);
 
-        veiculos.forEach(v -> JOptionPane.showMessageDialog(null, v));
+        veiculos.forEach(v -> JOptionPane.showMessageDialog(null, v, "VEICULO", JOptionPane.INFORMATION_MESSAGE));
     }
 }
