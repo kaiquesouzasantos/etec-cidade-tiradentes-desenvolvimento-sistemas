7650cd97ac4ab5e74bc935ad8c4947ed8c4e5e48
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-10-13 00:47:21 -0300

    Delete SpringCadastro/src directory

--- SpringCadastro/src/main/java/com/springcadastro/Repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springcadastro.Repository;
-
-import com.springcadastro.Model.Cliente;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepository extends MongoRepository<Cliente, String>{}
