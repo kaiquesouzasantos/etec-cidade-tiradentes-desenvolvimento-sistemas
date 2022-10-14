e55464d44dc4f9df9a42a0e71da18f1c793112b2
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-10-14 13:42:00 -0300

    Add files via upload

--- SpringCadastro/src/main/java/com/springcadastro/Component/Factory/GeradorListagem.java
+++ SpringCadastro/src/main/java/com/springcadastro/Component/Factory/GeradorListagem.java
@@ -3,70 +3,48 @@ package com.springcadastro.Component.Factory;
 import com.springcadastro.Model.*;
 import java.io.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GeradorListagem {
-    private String CABECALHO, ENDERECO = System.getProperty("user.home") + "\\Downloads\\";
+    private String CABECALHO, LISTA_STRING, ENDERECO = System.getProperty("user.home") + "\\Downloads\\";
 
-    public void geraArquivoListagemCliente(List<Cliente> lista) {
+    public void geraArquivoListagemCliente(List<Cliente> lista){
         ENDERECO += "listaDeClientes.csv";
         CABECALHO = "ID;NOME;CPF;EMAIL;TELEFONE;CEP;ESTADO";
+        LISTA_STRING = lista.stream().map(Cliente::formataObjeto).collect(Collectors.joining("\n"));
 
-        try {
-            PrintWriter escreve = new PrintWriter(new FileWriter(ENDERECO));
-            escreve.println(CABECALHO);
-
-            for (Cliente item : lista) {
-                escreve.println(item.formataObjeto());
-            }
-
-            escreve.close();
-        } catch (Exception ignored) {}
+        geraArquivoListagem();
     }
 
-    public void geraArquivoListagemFornecedor(List<Fornecedor> lista) {
+    public void geraArquivoListagemFornecedor(List<Fornecedor> lista){
         ENDERECO += "listaDeFornecedores.csv";
         CABECALHO = "ID;RAZAO;CNPJ;EMAIL;SITE;CEP;ESTADO";
+        LISTA_STRING = lista.stream().map(Fornecedor::formataObjeto).collect(Collectors.joining("\n"));
 
-        try {
-            PrintWriter escreve = new PrintWriter(new FileWriter(ENDERECO));
-            escreve.println(CABECALHO);
-
-            for (Fornecedor item : lista) {
-                escreve.println(item.formataObjeto());
-            }
-
-            escreve.close();
-        } catch (Exception ignored) {}
+        geraArquivoListagem();
     }
 
-    public void geraArquivoListagemProduto(List<Produto> lista) {
+    public void geraArquivoListagemProduto(List<Produto> lista){
         ENDERECO += "listaDeProdutos.csv";
         CABECALHO = "ID;NOME;ESPECIE;PRECO;QUANTIDADE;FORNECEDOR;DESCRICAO";
+        LISTA_STRING = lista.stream().map(Produto::formataObjeto).collect(Collectors.joining("\n"));
 
-        try {
-            PrintWriter escreve = new PrintWriter(new FileWriter(ENDERECO));
-            escreve.println(CABECALHO);
-
-            for (Produto item : lista) {
-                escreve.println(item.formataObjeto());
-            }
-
-            escreve.close();
-        } catch (Exception ignored) {}
+        geraArquivoListagem();
     }
 
-    public void geraArquivoListagemVenda(List<Venda> lista) {
+    public void geraArquivoListagemVenda(List<Venda> lista){
         ENDERECO += "listaDeVendas.csv";
         CABECALHO = "PRODUTO;CLIENTE;FORNECEDOR;SUBTOTAL;DATA";
+        LISTA_STRING = lista.stream().map(Venda::formataObjeto).collect(Collectors.joining("\n"));
 
+        geraArquivoListagem();
+    }
+
+    private void geraArquivoListagem() {
         try {
             PrintWriter escreve = new PrintWriter(new FileWriter(ENDERECO));
             escreve.println(CABECALHO);
-
-            for (Venda item : lista) {
-                escreve.println(item.formataObjeto());
-            }
-
+            escreve.print(LISTA_STRING);
             escreve.close();
         } catch (Exception ignored) {}
     }
