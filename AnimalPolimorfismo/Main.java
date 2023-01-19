97e8c28cf5ded83e45ab9f3f5c1eb51318f0465a
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-01-19 13:59:53 -0300

    Add files via upload

--- AnimalPolimorfismo/Main.java
+++ AnimalPolimorfismo/Main.java
@@ -5,34 +5,34 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        int opcao = Integer.parseInt(JOptionPane.showInputDialog(
-                null, "[1] - Elefante | [2] - Cachorro | [3] - Jaguar\n\nDIGITE QUAL ANIMAL DESEJA CADASTRAR: ",
-                "MENU", JOptionPane.QUESTION_MESSAGE
-        ));
+        int opcao = Integer.parseInt(exibePerguntaRecebeResposta("[1] - Elefante | [2] - Cachorro | [3] - Jaguar\n\nDIGITE QUAL ANIMAL DESEJA CADASTRAR: ", "MENU"));
+
         if(opcao != 1 && opcao != 2 && opcao != 3) {
-            JOptionPane.showMessageDialog(
-                    null, "OPCAO INVALIDA!", "MENU", JOptionPane.ERROR_MESSAGE);
+            exibeMensagem("OPCAO INVALIDA", "MENU", JOptionPane.ERROR_MESSAGE);
             System.exit(0);
         }
 
-        String nome = JOptionPane.showInputDialog(
-                null, "DIGITE O NOME DO ANIMAL: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE);
-        String cor = JOptionPane.showInputDialog(
-                null, "DIGITE A COR: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE);
-        double peso = Double.parseDouble(JOptionPane.showInputDialog(
-                null, "DIGITE O PESO EM KG(QUILOGRAMA): ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE));
+        String nome = exibePerguntaRecebeResposta("DIGITE O NOME DO ANIMAL: ", "INFORMACAO");
+        String cor = exibePerguntaRecebeResposta("DIGITE A COR DO ANIMAL: ", "INFORMACAO");
+        double peso = Double.parseDouble(exibePerguntaRecebeResposta("DIGITE O PESO EM KG(QUILOGRAMA): ", "INFORMACAO"));
 
         if(opcao == 1)
-            apresentaAnimal(new Elefante(nome, cor, peso));
+            exibeMensagem(new Elefante(nome, cor, peso).toString(), "ANIMAL");
         else if(opcao == 2)
-            apresentaAnimal(new Cachorro(nome, cor, peso));
+            exibeMensagem(new Cachorro(nome, cor, peso).toString(), "ANIMAL");
         else
-            apresentaAnimal(new Jaguar(nome, cor, peso));
+            exibeMensagem(new Jaguar(nome, cor, peso).toString(), "ANIMAL");
     }
 
-    public static void apresentaAnimal(Animal animal){
-        JOptionPane.showMessageDialog(
-                null, animal, "ANIMAL", JOptionPane.INFORMATION_MESSAGE
-        );
+    private static String exibePerguntaRecebeResposta(String message, String title){
+        return JOptionPane.showInputDialog(null, message, title, JOptionPane.QUESTION_MESSAGE);
+    }
+
+    private static void exibeMensagem(String message, String title){
+        JOptionPane.showMessageDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private static void exibeMensagem(String message, String title, int option){
+        JOptionPane.showMessageDialog(null, message, title, option);
     }
 }
