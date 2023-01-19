d7dd8df9f4b2c6063dbd1e0e4176d7ace5d9ad8a
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-01-19 13:16:57 -0300

    Add files via upload

---
+++ ContaPolimorfismoRefatorado/Entidades/ContaCorrente.java
@@ -0,0 +1,43 @@
+package ContaPolimorfismoRefatorado.Entidades;
+
+public class ContaCorrente extends Conta{
+    private double limite;
+
+    public ContaCorrente(){}
+    public ContaCorrente(double saldoConta, double limite, int agenciaConta, int numConta, int senha, String titularConta, String cpf) {
+        super(saldoConta, agenciaConta, numConta, senha, titularConta, cpf);
+        this.setLimite(limite);
+    }
+
+    public double getLimite() {return limite;}
+    public void setLimite(double limite) {this.limite = limite;}
+
+    @Override
+    public String sacar(double saqueValor) {
+        if(saqueValor <= getSaldoConta()) {
+            setSaldoConta(getSaldoConta() - saqueValor);
+            return  "--------------------------------------------------------------------------\n" +
+                    "SAQUE CONCLUIDO COM SUCESSO" +
+                    "\n--------------------------------------------------------------------------";
+        }else if(saqueValor <= getSaldoConta() + limite){
+            saqueValor = getSaldoConta() - saqueValor;
+            setSaldoConta(0);
+            setLimite(getLimite() - saqueValor);
+            return  "--------------------------------------------------------------------------\n" +
+                    "SAQUE CONCLUIDO COM SUCESSO" +
+                    "\n--------------------------------------------------------------------------";
+        }
+        return "--------------------------------------------------------------------------\n" +
+                "VALOR INDISPONIVEL" +
+                "\n--------------------------------------------------------------------------";
+    }
+
+    public String toString() {
+        return  "TITULAR: "+getTitularConta()+
+                "\nCPF: "+getCpf()+
+                "\nAGENCIA: "+getAgenciaConta()+
+                "\nCONTA: "+getNumConta()+
+                "\nSALDO: R$"+getSaldoConta()+
+                "\nLIMITE: R$"+limite;
+    }
+}
