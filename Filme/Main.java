6b7cef552eff1b035acfcd7e234250d8c9a9ffb7
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-01-19 14:18:54 -0300

    Update Main.java

--- Filme/Main.java
+++ Filme/Main.java
@@ -7,15 +7,16 @@ public class Main {
         StringBuilder saida = new StringBuilder();
 
         for(int i = 0; i < 8; i++){
-            filme.setTituloFilme(JOptionPane.showInputDialog(
-                    null, "Digite o nome do filme: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE));
-            filme.setDuracaoEmMinutos(Integer.parseInt(JOptionPane.showInputDialog(
-                    null, "Digite a duracao do filme: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE)));
+            filme.setTituloFilme(exibePerguntaRecebeResposta("Digite o nome do filme: "));
+            filme.setDuracaoEmMinutos(Integer.parseInt(exibePerguntaRecebeResposta("Digite a duracao do filme: ")));
 
             saida.append(filme.exibirDuracaoEmHoras()+"  |  "+filme.getTituloFilme()+"\n");
         }
 
-        JOptionPane.showMessageDialog(
-                null, "Os filme em cartazs sao: \n\nDURACAO | NOME\n\n"+saida, "FILMES", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Os filme em cartazs sao: \n\nDURACAO | NOME\n\n"+saida, "FILMES", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private static String exibePerguntaRecebeResposta(String message){
+        return JOptionPane.showInputDialog(null, message, "INFORMACAO", JOptionPane.QUESTION_MESSAGE);
     }
 }
