da4a869555f94a00fd1cbabffb7126bca342f2bb
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-01-19 17:02:21 -0300

    Add files via upload

--- FuncionarioHeranca/Main.java
+++ FuncionarioHeranca/Main.java
@@ -4,40 +4,48 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        int tipoFuncionario = Integer.parseInt(JOptionPane.showInputDialog(
-                null, "[1] - FUNCIONARIO\n[2] - GERENTE\n\nDIGITE O TIPO DE CONTA: ",
-                "MENU", JOptionPane.QUESTION_MESSAGE
-        ));
+        int tipoFuncionario = Integer.parseInt(exibePerguntaRecebeResposta("[1] - FUNCIONARIO\n[2] - GERENTE\n\nDIGITE O TIPO DE CONTA: "));
 
         if(tipoFuncionario != 1 && tipoFuncionario != 2){
-            JOptionPane.showMessageDialog(
-                    null, "OPCAO INVALIDA!", "MENU", JOptionPane.ERROR_MESSAGE);
+            exibeMensagem("OPCAO INVALIDA!", "MENU", JOptionPane.ERROR_MESSAGE);
             System.exit(0);
         }
 
-        String nomeFuncionario = JOptionPane.showInputDialog("DIGITE SEU NOME: ");
-        String cpfFuncionario = JOptionPane.showInputDialog("DIGITE SEU CPF: ");
-        double salarioFuncionario = Double.parseDouble(JOptionPane.showInputDialog("DIGITE SEU SALARIO: R$"));
+        String nomeFuncionario = exibePerguntaRecebeResposta("DIGITE SEU NOME: ");
+        String cpfFuncionario = exibePerguntaRecebeResposta("DIGITE SEU CPF: ");
+        double salarioFuncionario = Double.parseDouble(exibePerguntaRecebeResposta("DIGITE SEU SALARIO: R$"));
 
         if(tipoFuncionario == 1){
-            Funcionario funcionario = new Funcionario(nomeFuncionario, cpfFuncionario, salarioFuncionario);
-
-            JOptionPane.showMessageDialog(
-                    null, "VALOR COMISSAO: R$"+funcionario.valorComissao(),
-                    "COMISSAO", JOptionPane.INFORMATION_MESSAGE
+            exibeMensagem(
+                    "VALOR COMISSAO: R$" + new Funcionario(nomeFuncionario, cpfFuncionario, salarioFuncionario).valorComissao(),
+                    "COMISSAO"
             );
 
         }else{
             Gerente gerente = new Gerente(nomeFuncionario, cpfFuncionario, salarioFuncionario);
 
-            int senha = Integer.parseInt(JOptionPane.showInputDialog("DIGITE A SENHA: "));
+            int senha = Integer.parseInt(exibePerguntaRecebeResposta("DIGITE A SENHA: "));
             gerente.setSenha(12345);
 
             if(gerente.autenticaSenha(senha))
-                JOptionPane.showMessageDialog(
-                        null, "VALOR COMISSAO: R$"+gerente.valorComissao(),
-                        "COMISSAO", JOptionPane.INFORMATION_MESSAGE
+                exibeMensagem(
+                        "VALOR COMISSAO: R$"+gerente.valorComissao(), "COMISSAO"
                 );
+            else {
+                exibeMensagem("SENHA INCORRETA!", "AVISO", JOptionPane.ERROR_MESSAGE);
+            }
         }
+    }
+
+    private static String exibePerguntaRecebeResposta(String message){
+        return JOptionPane.showInputDialog(null, message, "INFORMACAO", JOptionPane.QUESTION_MESSAGE);
+    }
+
+    public static void exibeMensagem(String message, String title){
+        JOptionPane.showMessageDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void exibeMensagem(String message, String title, int option){
+        JOptionPane.showMessageDialog(null, message, title, option);
     }
 }
