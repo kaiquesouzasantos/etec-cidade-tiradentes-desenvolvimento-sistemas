2f15c3d79ca2f7ecb1c94cb148d45e685bd52729
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-01-19 17:24:20 -0300

    Update Main.java

--- GeometriaPolimorfismo/Main.java
+++ GeometriaPolimorfismo/Main.java
@@ -5,20 +5,16 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Main {
-    private static List<FiguraGeometrica> figuras = new ArrayList<>();
+    private static final List<FiguraGeometrica> figuras = new ArrayList<>();
 
     public static void main(String[] args) {
         int continua;
 
         do{
-            int opcao = Integer.parseInt(JOptionPane.showInputDialog(
-                    null, "[1] - Ciruculo\n[2] - Retangulo\n[3] - Losangulo\n[4] - Triangulo\n[5] - Quadrado\n\nDIGITE A FIGURA QUE DESEJA CALCULAR:",
-                    "MENU", JOptionPane.QUESTION_MESSAGE
-            ));
+            int opcao = Integer.parseInt(exibePerguntaRecebeResposta("[1] - Ciruculo\n[2] - Retangulo\n[3] - Losangulo\n[4] - Triangulo\n[5] - Quadrado\n\nDIGITE A FIGURA QUE DESEJA CALCULAR: "));
 
             if (opcao != 1 && opcao != 2 && opcao != 3 && opcao != 4 && opcao != 5)
-                JOptionPane.showMessageDialog(
-                        null, "FIGURA INVALIDA!", "MENU", JOptionPane.ERROR_MESSAGE);
+                exibeMensagem("FIGURA INVALIDA!", "MENU", JOptionPane.ERROR_MESSAGE);
             else
                 executaFigura(opcao);
 
@@ -29,25 +25,37 @@ public class Main {
         for (FiguraGeometrica n : figuras) {
             saida += n + "\n";
         }
-        JOptionPane.showMessageDialog(null, saida, "FIGURA GEOMETRICA", JOptionPane.INFORMATION_MESSAGE);
+        
+        exibeMensagem(saida, "FIGURA GEOMETRICA");
     }
 
     private static void executaFigura(int opcao){
         if(opcao == 1){
-            figuras.add(new Circulo(Double.parseDouble(JOptionPane.showInputDialog(
-                    null, "DIGITE O RAIO: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE))));
+            figuras.add(new Circulo(Double.parseDouble(exibePerguntaRecebeResposta("DIGITE O RAIO: "))));
         }else if(opcao == 5) {
-            figuras.add(new Quadrado(Double.parseDouble(JOptionPane.showInputDialog(
-                    null, "DIGITE O TAMANHO DO LADO: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE))));
+            figuras.add(new Quadrado(Double.parseDouble(exibePerguntaRecebeResposta("DIGITE O TAMANHO DO LADO: "))));
         }else{
-            double largura = Double.parseDouble(JOptionPane.showInputDialog(
-                    null, "DIGITE A LARGURA: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE));
-            double altura = Double.parseDouble(JOptionPane.showInputDialog(
-                    null, "DIGITE A ALTURA: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE));
+            double largura = Double.parseDouble(exibePerguntaRecebeResposta("DIGITE A LARGURA: "));
+            double altura = Double.parseDouble(exibePerguntaRecebeResposta("DIGITE A ALTURA: "));
 
-            if(opcao == 2) figuras.add(new Retangulo(altura, largura));
-            else if(opcao == 3) figuras.add(new Losangulo(altura, largura));
-            else figuras.add(new Triangulo(altura, largura));
+            if(opcao == 2)
+                figuras.add(new Retangulo(altura, largura));
+            else if(opcao == 3)
+                figuras.add(new Losangulo(altura, largura));
+            else
+                figuras.add(new Triangulo(altura, largura));
         }
+    }
+
+    private static String exibePerguntaRecebeResposta(String message){
+        return JOptionPane.showInputDialog(null, message, "INFORMACAO", JOptionPane.QUESTION_MESSAGE);
+    }
+
+    private static void exibeMensagem(String message, String title){
+        JOptionPane.showMessageDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private static void exibeMensagem(String message, String title, int option){
+        JOptionPane.showMessageDialog(null, message, title, option);
     }
 }
