0b699268fda71eaa7ff9218808f4510f4e97b68d
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-01-19 14:29:02 -0300

    Update Main.java

--- PessoaRevisao/Main.java
+++ PessoaRevisao/Main.java
@@ -5,15 +5,15 @@ public class Main {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
 
-        pessoa.setNome(JOptionPane.showInputDialog(
-                null, "DIGITE SEU NOME: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE));
-        pessoa.setAltura(Double.parseDouble(JOptionPane.showInputDialog(
-                null, "DIGITE SUA ALTURA EM METROS: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE)));
-        String frase = JOptionPane.showInputDialog(
-                null, "DIGITE UMA FRASE: ", "INFORMACAO", JOptionPane.QUESTION_MESSAGE);
-        pessoa.setFrase(frase);
+        pessoa.setNome(exibePerguntaRecebeResposta("DIGITE SEU NOME: "));
+        pessoa.setAltura(Double.parseDouble(exibePerguntaRecebeResposta("DIGITE SUA ALTURA EM METROS: ")));
+        pessoa.setFrase(exibePerguntaRecebeResposta("DIGITE UMA FRASE: "));
 
         JOptionPane.showMessageDialog(
                 null, pessoa.retornaAtributos(), "PESSOA", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private static String exibePerguntaRecebeResposta(String message){
+        return JOptionPane.showInputDialog(null, message, "INFROMACAO", JOptionPane.QUESTION_MESSAGE);
     }
 }
