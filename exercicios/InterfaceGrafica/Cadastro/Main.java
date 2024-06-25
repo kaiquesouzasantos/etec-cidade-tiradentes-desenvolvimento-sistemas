1cc5a85ad3bfa86003a05ea5c0e1ae0f84b7c2a1
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2024-06-25 15:09:29 -0300

    Update Main.java

--- exercicios/InterfaceGrafica/Cadastro/Main.java
+++ exercicios/InterfaceGrafica/Cadastro/Main.java
@@ -1,7 +1,7 @@
 package InterfaceGrafica.Cadastro;
 import InterfaceGrafica.Cadastro.Componentes.TelaInicio;
 
-public class TesteCadastro {
+public class Main {
     public static void main(String[] args) {
         new TelaInicio();
     }
